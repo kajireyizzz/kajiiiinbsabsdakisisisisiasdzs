c389b896cd3b0293d3222399390a828c677e1cde
kajireyizzz <39592492+kajireyizzz@users.noreply.github.com>
2018-05-24 16:51:48 +0300

    Create sarıl.js

---
+++ komutlar/sarıl.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const kill = require('../oldur.json');
+exports.run = function(client, message, args) {
+
+    var user;
+    let member = message.mentions.users.first();
+        let author = message.author;
+        if(member) {
+             user = member;
+        } else {
+             user = author;
+        }{
+        } member = message.guild.member(user);
+
+    const saril=new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .addField(`${message.author.username}`,`${user}`+ " adlı kullanıcıya sarıldı!")
+    .setImage('https://media3.giphy.com/media/10BcGXjbHOctZC/giphy.gif')
+    .setFooter("OA Premium")
+    return message.channel.send(saril);
+
+
+
+};
+
+
+
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'sarıl',
+  description: 'Etiketlenen kişiye sarılır.',
+  usage: 'sarıl <kişi>'
+};
