6883df19300810bc17e1d191640d20440221bfa8
kajireyizzz <39592492+kajireyizzz@users.noreply.github.com>
2018-05-26 01:18:41 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,11 @@
 const ayarlar = require('../ayarlar.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = ayarlar.prefix;
+
+
+  
+
+
 
 module.exports = member => {
     let username = member.user.username;
@@ -11,8 +15,10 @@ module.exports = member => {
     if (!channel) return;
     const embed = new Discord.RichEmbed()
     .setColor('#4cff00')
-    .setTitle(`@${member.user.username}#${member.user.discriminator} Sunucuya katıldı.`)
-    .setThumbnail(member.user.avatarURL)
+    .setDescription(`Sunucuya katıldı`)
+    .setAuthor(member.user.tag, member.user.avatarURL || member.user.defaultAvatarURL)
+    .setTitle(`@${member.user.username} #${member.user.discriminator} Sunucuya katıldı,[${member.guild.memberCount} üye]!.`)
+    .setThumbnail(member.user.avatarURL || member.user.defaultAvatarURL)
   
     
     .setTimestamp()
