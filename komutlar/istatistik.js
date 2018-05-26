87d553ca6b7351b882faf301913796e354d35100
kajireyizzz <39592492+kajireyizzz@users.noreply.github.com>
2018-05-26 17:32:19 +0300

    Update istatistik.js

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -6,7 +6,7 @@ const moment = require("moment");
 exports.run = (client, message) => {
   
 message.channel.send("İstatistikler hesaplanıyor...")
-.then(nmsg => nmsg.edit(new Discord.RichEmbed().addField("❯  İşletim istemi:","Linux").addField('❯  Sürümler:','Discord.JS:  v'+Discord.version+'\nBot:  v0.1.1').addField('❯  Bellek kullaımı:',(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)+' MB').addField('❯  Genel istatistikler','•Sunucu sayısı: '+ client.guilds.size.toLocaleString()+'\n•Kişi sayısı:  '+ client.users.size+'\n•Kanal sayısı: '+client.channels.size.toLocaleString()+'\n•Ping:  '+client.ping).setColor('RANDOM').setAuthor('İstatistikler',client.user.avatarURL).setThumbnail(client.user.avatarURL).setFooter('OA Premium |@Penia#9753 tarafından geliştirildi. ',client.user.avatarURL).setTimestamp())
+.then(nmsg => nmsg.edit(new Discord.RichEmbed().addField("❯  İşletim sistemi:","Linux").addField('❯  Sürümler:','Discord.JS:  v'+Discord.version+'\nBot:  v0.1.1').addField('❯  Bellek kullaımı:',(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)+' MB').addField('❯  Genel istatistikler','•Sunucu sayısı: '+ client.guilds.size.toLocaleString()+'\n•Kişi sayısı:  '+ client.users.size+'\n•Kanal sayısı: '+client.channels.size.toLocaleString()+'\n•Ping:  '+client.ping).setColor('RANDOM').setAuthor('İstatistikler',client.user.avatarURL).setThumbnail(client.user.avatarURL).setFooter('OA Premium |@Penia#9753 tarafından geliştirildi. ',client.user.avatarURL).setTimestamp())
 )};
 exports.conf = {
   enabled: true,
