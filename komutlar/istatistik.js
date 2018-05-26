5b0efb4c7dd08c67e652ac01b38819dc1debd9c6
kajireyizzz <39592492+kajireyizzz@users.noreply.github.com>
2018-05-26 17:26:22 +0300

    Update istatistik.js

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -1,24 +1,22 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require("discord.js");
 const moment = require("moment");
 
+require("moment-duration-format");
 
-exports.run = (client, message, params) => {
-message.channel.send("Bu komut devredışı bırakılmıştır.!");
-
-
-
-};
-
+exports.run = (client, message) => {
+  const duration = moment.duration(client.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]");
+message.channel.send("İstatistikler hesaplanıyor...")
+.then(nmsg => nmsg.edit(new Discord.RichEmbed().addField("❯  İşletim istemi:","Linux").addField('❯  Açık kalma süresi: ',`${duration}`).addField('❯  Sürümler:','Discord.JS:  v'+Discord.version+'\nBot:  v0.1.1').addField('❯  Bellek kullaımı:',(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)+' MB').addField('❯  Genel istatistikler','•Sunucu sayısı: '+ client.guilds.size.toLocaleString()+'\n•Kişi sayısı:  '+ client.users.size+'\n•Kanal sayısı: '+client.channels.size.toLocaleString()+'\n•Ping:  '+client.ping).setColor('RANDOM').setAuthor('İstatistikler',client.user.avatarURL).setThumbnail(client.user.avatarURL).setFooter('OA Premium |@Penia#9753 tarafından geliştirildi. ',client.user.avatarURL).setTimestamp())
+)};
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [],
+  aliases: ['bot durum', 'i', 'bi', 'istatistikler', 'kullanımlar', 'botdurum', 'bd', 'istatisik', 'stats', 'stat'],
   permLevel: 0
 };
 
 exports.help = {
-  name: 'istatistik' || 'i',
-  description: 'Bot`un istatistik bilgierini verir.',
+  name: 'istatistik',
+  description: 'Botun istatistik gösterir.',
   usage: 'istatistik'
 };
