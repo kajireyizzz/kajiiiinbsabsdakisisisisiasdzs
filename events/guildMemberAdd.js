2289500b379936b7b96c1d6087f6b83c7b72b44f
kajireyizzz <39592492+kajireyizzz@users.noreply.github.com>
2018-05-26 17:42:07 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -16,7 +16,7 @@ module.exports = member => {
     const embed = new Discord.RichEmbed()
     .setColor('#4cff00')
     
-    .setAuthor("Yeni Üye", member.user.avatarURL || member.user.defaultAvatarURL)
+    .setAuthor("Yeni Üye.!", member.user.avatarURL || member.user.defaultAvatarURL)
     .setTitle(`@${member.user.username} #${member.user.discriminator} Sunucuya katıldı,[${member.guild.memberCount} üye]!.`)
     .setThumbnail(member.user.avatarURL || member.user.defaultAvatarURL)
    
