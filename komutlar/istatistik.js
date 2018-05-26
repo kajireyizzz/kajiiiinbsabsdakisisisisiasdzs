9facf29718f7bbdf3f5907d633e9c2ff855f7e69
kajireyizzz <39592492+kajireyizzz@users.noreply.github.com>
2018-05-26 17:30:45 +0300

    Update istatistik.js

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -1,12 +1,12 @@
 const Discord = require("discord.js");
 const moment = require("moment");
 
-require("moment-duration-format");
+
 
 exports.run = (client, message) => {
-  const duration = moment.duration(client.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]");
+  
 message.channel.send("İstatistikler hesaplanıyor...")
-.then(nmsg => nmsg.edit(new Discord.RichEmbed().addField("❯  İşletim istemi:","Linux").addField('❯  Açık kalma süresi: ',`${duration}`).addField('❯  Sürümler:','Discord.JS:  v'+Discord.version+'\nBot:  v0.1.1').addField('❯  Bellek kullaımı:',(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)+' MB').addField('❯  Genel istatistikler','•Sunucu sayısı: '+ client.guilds.size.toLocaleString()+'\n•Kişi sayısı:  '+ client.users.size+'\n•Kanal sayısı: '+client.channels.size.toLocaleString()+'\n•Ping:  '+client.ping).setColor('RANDOM').setAuthor('İstatistikler',client.user.avatarURL).setThumbnail(client.user.avatarURL).setFooter('OA Premium |@Penia#9753 tarafından geliştirildi. ',client.user.avatarURL).setTimestamp())
+.then(nmsg => nmsg.edit(new Discord.RichEmbed().addField("❯  İşletim istemi:","Linux").addField('❯  Sürümler:','Discord.JS:  v'+Discord.version+'\nBot:  v0.1.1').addField('❯  Bellek kullaımı:',(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)+' MB').addField('❯  Genel istatistikler','•Sunucu sayısı: '+ client.guilds.size.toLocaleString()+'\n•Kişi sayısı:  '+ client.users.size+'\n•Kanal sayısı: '+client.channels.size.toLocaleString()+'\n•Ping:  '+client.ping).setColor('RANDOM').setAuthor('İstatistikler',client.user.avatarURL).setThumbnail(client.user.avatarURL).setFooter('OA Premium |@Penia#9753 tarafından geliştirildi. ',client.user.avatarURL).setTimestamp())
 )};
 exports.conf = {
   enabled: true,
