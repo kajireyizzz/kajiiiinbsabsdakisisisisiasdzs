f5d0b3e7e582bbdf982e80d416b5cdc77fad325d
kajireyizzz <39592492+kajireyizzz@users.noreply.github.com>
2018-05-27 21:51:30 +0300

    Update profil.js

--- komutlar/profil.js
+++ komutlar/profil.js
@@ -1,65 +1,38 @@
 const Discord = require('discord.js');
-const moment = require('moment');
-const client = new Discord.Client();
+const ayarlar = require('../ayarlar.json');
 
-exports.run = (client, message, params,dateformat) => {
-
-    var user;
-    let member = message.mentions.users.first();
-        let author = message.author;
-        if(member) {
-             user = member;
-        } else {
-             user = author;
-        }
-    member = message.guild.member(user);
-    let roles = member.roles.array().slice(1).sort((a, b) => a.comparePositionTo(b)).reverse().map(role => role.name);
-       if (roles.length < 1) roles = ['Bu Kullanıcının Rolü Yok!'];
-    const millisCreated = new Date().getTime() - user.createdAt.getTime();
-    const daysCreated = moment.duration(millisCreated).format("Y [yıl], D [gün], H [saat], m [dakika], s [saniye]")
-    const millisJoined = new Date().getTime() - member.joinedAt.getTime();
-    const userJoined = moment.duration(millisJoined).format("Y [yıl], D [gün], H [saat], m [dakika], s [saniye]")
-    if(user.presence.status === "dnd"){
-      var durum = "Rahatsız Etmeyin"
-    }
-    else if(user.presence.status === "online"){
-      var durum = "Çevrimiçi"
-    }
-    else if(user.presence.status === "idle"){
-      var durum = "Boşta"
-    }
-      else {
-      var durum = "Görünmez"
-    }
-     const embed5 = new Discord.RichEmbed()
-         .setColor(Math.floor(Math.random() * (0xFFFFFF + 1)))
-         .setTimestamp()
-         .setThumbnail(`${user.displayAvatarURL}`)
-         .addField("Kullanıcı", `${user}`, true)
-         .addField("Şu anda oynadığı oyun", user.presence.game ? user.presence.game.name : 'Oynadığı bir oyun yok', true)
-         .addField("Durum", `${durum}` , true)
-         .addField('Katılım tarihi (Sunucu)', `${userJoined}`, true)
-         .addField('Katılım Tarihi (Discord)', `${daysCreated}`, true)
-         //.addField("Hesabın Kuruluş Tarihi", `${dateformat(user.createdAt)}`)
-         .addField('Bu sunucudaki rolleri', `${roles.join(', ')}`, true)
-         .setFooter(`OA Premium`);
-         message.channel.send({embed: embed5})
-
-
-
-
-
-  };
+exports.run = (client, message, args) => {
+  let guild = message.guild
+  let user = message.mentions.users.first();
+  if (message.mentions.users.size < 1) return message.reply('Kimin Kullanıcı Bilgisini İstediğini Yazmalısın').catch(console.error);
+  let mesaj = args.slice(0).join(' ');
+    if (message.channel.type !== "group") {
+        var userDurum = user.presence.status;
+        var userDurm = (userDurum == "online" ? (0x00AE86) : (userDurum == "offline" ? (0x808080) : (userDurum == "idle" ? (0xFFFF00) : (userDurum == "dnd" ? (0xFF0000) : (0x00AE86)))))
+        var userdurm = (userDurum == "online" ? ("Çevrimiçi") : (userDurum == "offline" ? ("Çevrimdışı") : (userDurum == "idle" ? ("Boşta") : (userDurum == "dnd" ? ("Rahatsız Etmeyin") : ("Bilinmiyor/bulunamadı.")))))
+      const kullanicibilgimk = new Discord.RichEmbed()
+      .setAuthor(message.author.username, message.author.avatarURL)
+      .setColor("RANDOM")
+      .setTimestamp()
+      .addField('Ad:', user.username + '#' + user.discriminator)
+      .addField('ID:', user.id)
+      .addField('Kayıt tarihi:', user.createdAt)
+      .addField('Durum:', userdurm)
+      .addField('Şu an oynadığı oyun:', user.presence.game ? user.presence.game.name : 'Şu an oyun oynamıyor')
+      .addField('BOT mu?', user.bot ? '\n Evet' : 'Hayır')
+      return message.channel.sendEmbed(kullanicibilgimk);
+  }
+};
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['p'],
+  aliases: ['profil', 'p'],
   permLevel: 0
 };
 
 exports.help = {
   name: 'profil',
-  description: 'Profiliniz hakkında bilgi verir.',
-  usage: 'profil'
+  description: 'Etiketlene kişinin hakkında bilgi verir.',
+  usage: 'profil <kullanıcı>'
 };
