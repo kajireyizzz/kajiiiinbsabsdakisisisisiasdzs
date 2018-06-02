3fc756568ff642c483a66ef9cfe19a1424643313
kajireyizzz <39592492+kajireyizzz@users.noreply.github.com>
2018-06-02 22:54:54 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -17,9 +17,22 @@ exports.run = (client, message, params) => {
   .addField(":book: Kullanıcı Komutları","**avatar**:Avatarınızı görüntüler.\n**bildir**:Rahatsız olduğunuz kişiyi adminlere bildirmenize yarar.\n**botbilgi**:Bot hakkında bilgi alırsınız.!\n**emojiler**:Sunucudaki mevcut emojileri görüntüler.\n**davet**:Sunucumuza arkadaşlarınızı davet etmeniz için bot size bir link verir.\n**ekip**:Yönetim ekibi listesini gösterir.\n**havadurumu**:Girilen şehir`in hava durumunu gösterir.\n**istatistik**:Bot istatistiklerini gösterir.\n**profil**:Etiketlenen kişinin yada sizin bilgilerinizi gösterir.\n**sunucubilgi**:Sunucu hakkında bilgi verir.\n**yenilikler**:Bota gelen yenilik ve güncellemeleri gösterir.\n**öner**:Bota eklenmesini istediğiniz tavsiye ve önerilerinizi yöneticilere iletir.\n**ping**:Botun anlık pingini gösterir.\n**reklamtaraması**:Oynuyor kısmında reklam taraması yapar.\n**roller**:Sunucudaki mevcut rolleri görüntüler.\n**yardım**:Botun komutlarını gösterir.")
   .addField(":hammer_pick: Admin ve Mod Komutları","**anket**:Sunucu içerisinde anket yapar.\n**ban**:Belirlenen kişi sunucudan yasaklar.!\n**dm**:Bot ile istenilen kullanıcıya bot tarafından mesaj gönderebilirsiniz.\n**kanalaç**:Yeni kanal açar.\n**kurulum**:Bot için gerekli kanalları kurar.\n**temizle**:Belirtilen miktarda mesaj siler.")
    .addField(":closed_lock_with_key: Kurucu(Penia)","**baslat**:Adı girilen komutu yeniden başlatır.\n**sunucular**:Botun bulunduğu sunucuları mesaj atar.\n**ayarla**:Botun oyununu ayarlar.\n**reboot**:Botu yeniden başlatır.")
-    message.author.send(yardımlistesi);
+    message.channel.send(yardımlistesi);
     //message.author.sendCode('asciidoc', `= Komut Listesi =\n\n[Komut hakkında bilgi için ${ayarlar.prefix}yardım <komut adı>]\n\n${client.commands.map(c => `${ayarlar.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`);
+  if (message.channel.type !== 'dm') {
+    const ozelmesajkontrol = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .addField("Özel mesajlarını kontrol et.!",'Komutları özel mesaj olarak yolladım. :postbox:');
+    message.channel.sendEmbed(ozelmesajkontrol) }
+  } else {
+    let command = params[0];
 
+
+
+
+  }
 };
 
 exports.conf = {
