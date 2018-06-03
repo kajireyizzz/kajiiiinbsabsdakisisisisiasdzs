21e561f109f207103bbe54b2b0412ab1235917a3
kajireyizzz <39592492+kajireyizzz@users.noreply.github.com>
2018-06-04 02:43:31 +0300

    Update yeni.js

--- komutlar/yeni.js
+++ komutlar/yeni.js
@@ -8,7 +8,7 @@ exports.run = (client, message, params) => {
        .setTitle("DEV GÜNCELLEME")
        .addField("Discord.JS-Commando sistemine geçiyoruz","Nedir bu Discord.JS-Commando ?\nDiscord`un standart sabit Discord.js modülünün daha geniş kapsamlı kodlara dönüşmesini ve bizlere kolaylık sağlayan bir tür modüldür.")
        .addField("Peki neler değişecek","Hemen hemen herşeyi en baştan sıfırdan değiştireceğim tek başıma her ne kadar yorucu olacaksada herşey sizler için.")
-       .addField("Ne zaman gelecek güncelleme","Yeni güncellemenin hemen hemen 3/1`lik bir kısmı bitmiş bulunmakta ancak yeni yeni bu frameworkü gördüğüm için yabancılık çekiyorum kodlara karşı en kısa zamanda elimden geldiğince hızlı bir şekilde bitirmeye çalışacağım.")
+       .addField("Ne zaman gelecek güncelleme","Yeni güncellemenin hemen hemen 3/1`lik bir kısmı bitmiş bulunmakta ancak yeni yeni bu frameworkü gördüğüm için yabancılık çekiyorum kodlara karşı en kısa zamanda elimden geldiğince hızlı bir şekilde bitirmeye çalışacağım.\nHepinize Sonsuz Teşekkürler **Penia**")
        .setFooter("DEV GÜNCELLEME")
    message.channel.send(ekip);
 };
